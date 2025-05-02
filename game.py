12a7bc445d5f3d32bd0f8ad3c2262e016b837f06
HumayeraShayera <35778937+Humayera-Shayera@users.noreply.github.com>
2025-05-02 21:11:16 +0600

    Update game.py

--- game.py
+++ game.py
@@ -86,7 +86,7 @@ def draw_text(x, y, text, font=GLUT_BITMAP_HELVETICA_18):
     glColor3f(1,1,1)
     glMatrixMode(GL_PROJECTION)
     glPushMatrix(); glLoadIdentity()
-    gluOrtho2D(0,1000,0,800)
+    gluOrtho2D(0,900,0,700)
     glMatrixMode(GL_MODELVIEW)
     glPushMatrix(); glLoadIdentity()
     glRasterPos2f(x,y)
