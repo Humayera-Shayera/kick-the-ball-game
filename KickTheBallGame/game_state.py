89a66fc184dbca7786af0e975c593d453a5af602
HumayeraShayera <35778937+Humayera-Shayera@users.noreply.github.com>
2025-05-02 11:25:42 +0600

    Create game_state.py

---
+++ KickTheBallGame/game_state.py
@@ -0,0 +1,24 @@
+player_pos = [0.0, -300.0, 0.0]
+player_rot = 0.0
+player_fall_ang = 0.0
+
+ball_pos = [0.0, 0.0, 0.0]
+ball_vel = [0.0, 0.0, 0.0]
+has_ball = False
+pickup_cooldown = 0
+
+life = 5
+score = 0
+game_over = False
+
+goal_flag = False
+goal_time = 0.0
+
+enemies = []
+ENEMY_COUNT = 5
+
+camera_mode = "third"
+camera_pos = (0.0, 500.0, 500.0)
+fovY = 120.0
+
+game_difficulty = "Medium"
