478182f3d3d14260e0eb9b2e279a06f936d9b54f
Abrar Amiya <115402065+AbrarAmiya@users.noreply.github.com>
2025-05-02 21:02:06 +0600

    Update Player & Opponent Color Customization.py

---
+++ KickTheBallGame/Player & Opponent Color Customization.py
@@ -0,0 +1,6 @@
+"player_color": (0.0, 1.0, 0.0),
+"enemy_color": (1.0, 0.0, 0.0)
+if key == b'1':
+    game_state["player_color"] = (1, 0, 0)  # Red
+elif key == b'2':
+    game_state["player_color"] = (0, 1, 0)  # Green
